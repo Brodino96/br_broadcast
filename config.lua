a98433364f58cd4416b84f83649e85bec2cb2db2
Vincent Acanfora <brodino96@gmail.com>
2024-11-11 12:57:16 +0100

    inital commit

---
+++ config.lua
@@ -0,0 +1,8 @@
+Config = {
+    -- Replace the array with [nil] in case you don't want to use that section
+    veh = {
+        types = { "automobile" }, -- automobile, bike, boat, heli, plane, submarine, trailer, trains
+        models = { "bati" }, -- Vehicle model name
+    },
+    items = { "WEAPON_PISTOL" } -- ox_inventory item name
+}
