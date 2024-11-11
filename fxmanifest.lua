4227a79a5164aaa54b2b8efe1f48ead7ce74d8d2
Vincent Acanfora <brodino96@gmail.com>
2024-11-11 12:45:30 +0100

    Initial commit

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,1 +1,15 @@
+fx_version "cerulean"
+game "gta5"
+lua54 "yes"
+
 author "Brodino"
+description "Broadcast a player voice to all the players in the server that respect the config"
+version "0.1"
+
+shared_scripts { "config.lua", }
+server_scripts { "server/*", }
+client_scripts { "client/*", }
+
+dependencies {
+    "pma-voice"
+}
